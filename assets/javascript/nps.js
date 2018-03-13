a4a35e54b3152d1e2ae14b22088a7a9ac01749b4
Alvin Ayusa <alvinayusa@alvins-mbp.lan>
2018-03-12 20:13:52 -0700

    js file integration

---
+++ assets/javascript/nps.js
@@ -0,0 +1,53 @@
+// <!-- START of Javascript -->
+$(document).ready(function () {
+
+    var markers = []
+    
+    // retrieve the JSON object for NPS
+    $("#enterbtn").on("click", function (event) {
+        event.preventDefault()
+
+        // variables
+        var parkCode = $("#parkCode-input").val()
+        var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=" + parkCode + "&fields=images&api_key=BiJRAIaZNmBh3eBVfZDeONL4s34x07I2eTRCeCBf"
+
+        // start of longlat ajax calls
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            // console.log(response.data)
+
+            var parkDetails = $("<div class = 'parkDetails'>").appendTo("#detailsDiv")
+            $("<div class = 'parkDetails'>").appendTo("#detailsDiv")
+
+            for (var i = 0; i < response.data.length; i++) {
+
+                var latLong = response.data[i].latLong
+                var parkCode = response.data[i].parkCode
+                var parkName = response.data[i].fullName
+
+                // console.log(latLong)
+                markers.push(
+                    {
+                        //pass any dynamic argument in the function when called. (lat lng from NPS)
+                        coords: latLong, //NPS API Info
+                        parkcode: parkCode, //NPS API info
+                        parkname: parkName//NPS API info
+                    }
+
+                )
+
+            // END of for loop function   
+            }
+
+            // END of longlat ajax calls
+        })
+
+        $.getScript("assets/javascript/app.js")
+        // END of click button function
+    })
+
+    
+    //END of document.ready
+})
